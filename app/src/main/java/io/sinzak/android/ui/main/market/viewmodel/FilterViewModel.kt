0f23a19ee50059cfb487027c0db006d9aaddf527
DivisonOfficer <wlssud0701@naver.com>
2022-12-11 14:50:10 +0900

    [12. 11 - 김진녕] Filter adapter redusable하게 정리

--- app/src/main/java/io/sinzak/android/ui/main/market/viewmodel/FilterViewModel.kt
+++ app/src/main/java/io/sinzak/android/ui/main/market/viewmodel/FilterViewModel.kt
@@ -11,48 +11,10 @@ import javax.inject.Inject
 @HiltViewModel
 class FilterViewModel @Inject constructor() : BaseViewModel() {
 
-    val adapter = FilterAdapter()
-
-    private val _filterList = MutableStateFlow(mutableListOf("회화일반","동양화","조소","판화","공예","기타"))
-    val filterList : StateFlow<MutableList<String>> get() = _filterList
-    private val _filterChosenList = MutableStateFlow(mutableListOf<String>())
-    val filterChosenList : StateFlow<MutableList<String>> get() = _filterChosenList
-
-    val onFilterClick : (String?,Boolean) -> Unit = {filter, status ->
-
-        filter?.run{
-            if(status)
-            {
-                val filters = mutableListOf<String>()
-                filters.addAll(filterList.value)
-                filters.remove(filter)
-                _filterList.value = filters
-                val filterCs = mutableListOf<String>()
-                filterCs.addAll(_filterChosenList.value)
-                filterCs.add(filter)
-                _filterChosenList.value = filterCs
-
-            }
-            else{
-                val filters = mutableListOf<String>()
-                filters.addAll(filterList.value)
-                filters.add(filter)
-                _filterList.value = filters
-                val filterCs = mutableListOf<String>()
-                filterCs.addAll(_filterChosenList.value)
-                filterCs.remove(filter)
-                _filterChosenList.value = filterCs
-            }
-        }?:run{
-            val filters = mutableListOf<String>()
-            filters.addAll(filterChosenList.value)
-            filters.addAll(filterList.value)
-            _filterList.value = filters
-            _filterChosenList.value = mutableListOf()
-        }
-
-
+    val adapter = FilterAdapter(mutableListOf("회화일반", "동양화", "조소", "판화", "공예", "기타")) {
 
     }
+
+
 
 }
