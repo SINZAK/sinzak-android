6d12b11381c27a4858c95ab0b19e8e789cd71b32
DivisonOfficer <41609506+DivisonOfficer@users.noreply.github.com>
2023-01-07 23:32:55 +0900

    [01.07 - 김진녕] 1. 사진 업로드 UI - 드래그 기능 포함

--- app/src/main/java/io/sinzak/android/ui/main/postwrite/adapter/ImageAdapter.kt
+++ app/src/main/java/io/sinzak/android/ui/main/postwrite/adapter/ImageAdapter.kt
@@ -4,7 +4,10 @@ import android.content.res.Resources
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.children
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -13,18 +16,26 @@ import io.sinzak.android.databinding.HolderWriteImgBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.lang.Exception
+import java.util.*
 
 class ImageAdapter(val img : MutableList<Uri>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
 
+    val touchHelperCallback = ItemTouchListener()
 
+
+    val isEmpty get() = img.isEmpty()
 
     override fun getItemCount(): Int {
-        return img.size
+        return if (isEmpty) 5
+        else img.size
     }
 
     override fun onBindViewHolder(holder: ImageAdapter.ViewHolder, position: Int) {
+        if(isEmpty){
+            holder.bindNull()
+            return
+        }
         holder.bindImg(img[position],position)
     }
 
@@ -37,17 +48,18 @@ class ImageAdapter(val img : MutableList<Uri>) : RecyclerView.Adapter<ImageAdapt
 
     inner class ViewHolder(val bind : HolderWriteImgBinding) : RecyclerView.ViewHolder(bind.root){
 
+        fun bindNull(){
+            bind.isFirst = false
+            bind.ivImg.setImageDrawable(bind.ivImg.context.getDrawable(R.drawable.ic_img_null_holder))
+
+        }
+
         fun bindImg(uri : Uri, idx : Int){
 
             bind.isFirst = idx == 0
 
             CoroutineScope(Dispatchers.Main).launch {
-
-
-
                 try{
-
-
                     Glide.with(bind.ivImg).asBitmap().load(uri).transform(RoundedCorners(
                         (Resources.getSystem().displayMetrics.density * 8f).toInt()
                     )).into(bind.ivImg)
@@ -55,13 +67,81 @@ class ImageAdapter(val img : MutableList<Uri>) : RecyclerView.Adapter<ImageAdapt
                 catch(e:Exception){
 
                 }
-
-
             }
+        }
+    }
 
 
+
+    inner class ItemTouchListener : ItemTouchHelper.Callback(){
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            val dragFlags = ItemTouchHelper.START or ItemTouchHelper.END
+            val swipeFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            return makeMovementFlags(dragFlags, swipeFlags)
+        }
+
+
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            val fromPosition = recyclerView.getChildAdapterPosition(viewHolder.itemView)
+            val toPosition = recyclerView.getChildAdapterPosition(target.itemView)
+
+            if (fromPosition < toPosition) {
+                for (i in fromPosition until toPosition) {
+                    Collections.swap(img, i, i + 1)
+                }
+            } else {
+                for (i in fromPosition downTo toPosition + 1) {
+                    Collections.swap(img, i, i - 1)
+                }
+            }
+            notifyItemMoved(fromPosition, toPosition)
+
+
+            adjustHolderNum(recyclerView)
+            return true
+        }
+
+        fun adjustHolderNum(rv : RecyclerView){
+            rv.children.forEach {
+                val order = rv.getChildAdapterPosition(it)
+                val vh = (rv.getChildViewHolder(it) as ViewHolder)
+                if(order == 0 && vh.bind.isFirst == false){
+                    vh.bind.isFirst = true
+                    vh.bind.notifyChange()
+                }
+
+                if(order != 0 && vh.bind.isFirst == true){
+                    vh.bind.isFirst = false
+                    vh.bind.notifyChange()
+                }
+            }
+        }
+
+
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
 
         }
 
+
+
+        override fun isLongPressDragEnabled(): Boolean {
+            return true
+        }
     }
+}
+
+@BindingAdapter("adapter")
+fun setAdapter(view : RecyclerView, adapter : ImageAdapter)
+{
+    view.adapter = adapter
+    ItemTouchHelper(adapter.touchHelperCallback).attachToRecyclerView(view)
 }
