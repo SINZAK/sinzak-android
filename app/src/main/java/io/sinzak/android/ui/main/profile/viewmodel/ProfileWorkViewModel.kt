9d10cdb965a538c5bcb61edb7a907fa2939d0f2d
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-01-25 00:34:39 +0900

    [01.25-안승우] 프로필 판매작품, 작업해요 연결중

--- app/src/main/java/io/sinzak/android/ui/main/profile/viewmodel/ProfileWorkViewModel.kt
+++ app/src/main/java/io/sinzak/android/ui/main/profile/viewmodel/ProfileWorkViewModel.kt
@@ -1,11 +1,21 @@
 package io.sinzak.android.ui.main.profile.viewmodel
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.sinzak.android.model.profile.ProfileModel
 import io.sinzak.android.ui.base.BaseViewModel
 import io.sinzak.android.ui.main.profile.adapter.ProfileArtAdapter
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileWorkViewModel @Inject constructor(): BaseViewModel() {
-    val adapter = ProfileArtAdapter()
+class ProfileWorkViewModel @Inject constructor(
+    val model : ProfileModel
+): BaseViewModel() {
+//    val adapter = ProfileArtAdapter(type = 1)
+//
+//    init {
+//        invokeStateFlow(model.workList){
+//            adapter.setWorkData(it)
+//        }
+//    }
+    val workList get() = model.workList
 }
