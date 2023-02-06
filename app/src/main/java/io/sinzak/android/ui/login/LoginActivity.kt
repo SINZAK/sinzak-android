23f78f4d8782c3eeb1c8e8e7098ad6e78f0c0c22
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-02-06 15:28:55 +0900

    [02.06-안승우] 안쓰는 import 제거

--- app/src/main/java/io/sinzak/android/ui/login/LoginActivity.kt
+++ app/src/main/java/io/sinzak/android/ui/login/LoginActivity.kt
@@ -1,16 +1,7 @@
 package io.sinzak.android.ui.login
 
-import android.app.Activity
 import android.content.Intent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
 import com.navercorp.nid.NaverIdLoginSDK
@@ -18,7 +9,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import io.sinzak.android.R
 import io.sinzak.android.databinding.ActivityLoginBinding
 import io.sinzak.android.system.LogDebug
-import io.sinzak.android.system.LogError
 import io.sinzak.android.system.social.NaverImpl
 import io.sinzak.android.ui.base.BaseActivity
 import javax.inject.Inject
@@ -49,7 +39,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
         viewModel.registerConnect(connect, this as BaseActivity<*>)
     }
 
-    fun observation(){
+    private fun observation(){
         invokeBooleanFlow(viewModel.signModel.sdkSignSuccess){
             showToast("소셜 로그인에 성공했습니다.")
         }
