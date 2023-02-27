796acef3cbff2ae0f9d3b06657d70587c4c6a136
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-02-27 20:31:38 +0900

    [02.27-안승우] 홈 검색 아이콘 gone

--- app/src/main/java/io/sinzak/android/ui/main/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/io/sinzak/android/ui/main/home/viewmodel/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package io.sinzak.android.ui.main.home.viewmodel
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.sinzak.android.enums.Page
 import io.sinzak.android.model.market.HomeProductModel
 import io.sinzak.android.ui.base.BaseViewModel
 import javax.inject.Inject
@@ -15,5 +16,10 @@ class HomeViewModel @Inject constructor(
     fun getProducts(){
         model.getProducts()
         model.getBanner()
+    }
+
+    fun gotoNotification(){
+        if (!signModel.isUserLogin()) return
+        navigation.changePage(Page.HOME_NOTIFICATION)
     }
 }
